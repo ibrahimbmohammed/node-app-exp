20c6fffbfd74f5dbb74c4b6931f13022ebfbaa1d
Mohammed Ibrahim B <46991697+ibrahimbmohammed@users.noreply.github.com>
2020-10-20 09:11:37 +0100

    adding knex

--- app.js
+++ app.js
@@ -8,6 +8,28 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+//DATABASE_URL
+
+//postgres://ahwmjspkxuhcjy:671505558274c9b405b9664f244990cd1e283b8013727bdcb88621a6cafecc1e@ec2-34-248-165-3.eu-west-1.compute.amazonaws.com:5432/d1ursnvoqhf7q8
+
+// const { Client } = require('pg');
+
+// const client = new Client({
+//   connectionString: process.env.DATABASE_URL,
+//   ssl: {
+//     rejectUnauthorized: false
+//   }
+// });
+
+// client.connect();
+
+// client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
+//   if (err) throw err;
+//   for (let row of res.rows) {
+//     console.log(JSON.stringify(row));
+//   }
+//   client.end();
+// });
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
